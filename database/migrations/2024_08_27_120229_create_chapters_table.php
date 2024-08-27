7082fe8fa8bc46c224aefd7dfa5dffa99c6c4224
= <tiagospem@gmail.com>
2024-08-27 09:04:05 -0300

    add chapters mfs

--- database/migrations/2024_08_27_120229_create_chapters_table.php
+++ database/migrations/2024_08_27_120229_create_chapters_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('topics', function (Blueprint $table) {
+        Schema::create('chapters', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description');
@@ -18,6 +18,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('topics');
+        Schema::dropIfExists('chapters');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             NewspaperSeeder::class,
             CaseStudySeeder::class,
             TopicSeeder::class,
+            ChaptersSeeder::class,
         ]);
     }
 }
